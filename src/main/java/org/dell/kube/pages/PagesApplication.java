6b85e89104f56340f1f9e621c57b93ec2a003964
sathyaem <sathiasara@gmail.com>
2021-08-08 12:58:32 +0100

    Pipeline 1.0

---
+++ src/main/java/org/dell/kube/pages/PagesApplication.java
@@ -0,0 +1,13 @@
+package org.dell.kube.pages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagesApplication.class, args);
+	}
+
+}
